3950cecb1d6eaaf55dbb6f8d81ec04704427d5bb
WENG JIA HONG <153618357+ahsbdcpu@users.noreply.github.com>
2024-03-19 12:11:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask import request, abort
-from linebot import  LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
@@ -24,23 +24,24 @@ def callback():
 
 @handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text1=event.message.text
-    user_ability = (
-        "職業":"醫生",
-        "技能":"救治病人",
+    text1 = event.message.text
+    user_ability = {
+        "職業": "醫生",
+        "技能": "救治病人",
+    }
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "user", "content": text1}
+            {"role": "user", "content": text1},
             {"role": "system", "content": "這是GPT的個性資訊 " + str(user_ability)}
         ],
         model="gpt-3.5-turbo-0125",
-        temperature = 0.5,
+        temperature=0.5,
     )
     try:
         ret = response['choices'][0]['message']['content'].strip()
     except:
         ret = '發生錯誤！'
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=ret))
 
 if __name__ == '__main__':
     app.run()
